5e57866407dd1a791f57636c77c3d5d81df681fc
Sabrina Hennrich <sabrina@digimedialoop.de>
2025-02-20 14:45:25 +0100

    grid fix

--- pages/wissenswertes/index.vue
+++ pages/wissenswertes/index.vue
@@ -87,11 +87,11 @@ watch(news, (newVal) => {
 
     .grid
         display: grid
-        grid-template-columns: repeat(auto-fill, minmax(280px, 1fr))
-        gap: 20px
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr))
+        gap: 2rem
         justify-content: start
         width: 100%
-        max-width: 1200px
+        max-width: 100%
         margin: 0 auto
 
     .article
